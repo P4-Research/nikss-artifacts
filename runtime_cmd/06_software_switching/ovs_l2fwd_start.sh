8c83b755b6bcd83750924411e1d8ae010523c1b1
Tomasz Osinski <osinstom@gmail.com>
2021-11-20 10:39:51 +0100

    Fix OVS L2FWD

--- runtime_cmd/06_software_switching/ovs_l2fwd_start.sh
+++ runtime_cmd/06_software_switching/ovs_l2fwd_start.sh
@@ -1,10 +1,12 @@
 ovs-vsctl del-br br0
+ovs-vsctl add-br br0
 
-ovs-vsctl add-br br0 
+ovs-vsctl add-port br0 $PORT0_NAME
+ovs-vsctl add-port br0 $PORT1_NAME
 
-ovs-vsctl add-port br0 $PORT0_NAME 
-ovs-vsctl add-port br0 $PORT1_NAME 
+ovs-ofctl del-flows br0
 
-ovs-ofctl del-flows br0 
+ovs-ofctl add-flow br0 "eth_dst=$GENERATOR_MAC1, actions=output:$PORT1_NAME"
+ovs-ofctl add-flow br0 "eth_dst=$GENERATOR_MAC0, actions=output:$PORT0_NAME"
 
-ovs-ofctl add-flow br0 "eth_dst=$GENERATOR_MAC1, actions=output:$PORT1_INDEX"
+ovs-ofctl dump-flows br0

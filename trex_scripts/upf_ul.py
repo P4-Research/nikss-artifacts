4f0d0f378b129bcb76593a42324c4c9e8269fc8d
Tomasz Osiński <osinstom@gmail.com>
2021-12-15 19:28:51 +0100

    Update upf_ul.py

--- trex_scripts/upf_ul.py
+++ trex_scripts/upf_ul.py
@@ -5,14 +5,13 @@ class STLS1(object):
 
 
     def create_stream (self,  packet_len):
+        size = packet_len - 4
         packet = Ether()/IP(src="172.20.16.99",dst="172.20.16.105")/UDP(dport=2152)/GTP_U_Header(teid=1234)/IP(src="10.10.10.10",dst="192.168.2.21",version=4,id=0xFFFF)/UDP(sport=99,dport=99)
-        packet /= 'x' * packet_len
-        pkt = STLPktBuilder(pkt = packet, vm = [])
+        pad = max(0, size - len(packet)) * 'x'
+        pkt = STLPktBuilder(pkt = packet/pad, vm = [])
         return STLStream(packet = pkt, mode = STLTXCont())
 
-
     def get_streams (self, direction = 0,  packet_len=64, **kwargs):
-        packet_len=max(0,packet_len-42)
         return [ self.create_stream(packet_len) ]
 
 
